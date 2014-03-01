55b049b4ac5ece06dc8271c4e1fb9715c540ac01
DirectoriX <dontsov.mih@yandex.ru>
2014-03-02 01:17:41 +0400

    Удалены неиспользуемые операторы импорта из SQLAgent.java

--- TradingTerminal/src/java/org/aba/tradingterminal/SQLAgent.java
+++ TradingTerminal/src/java/org/aba/tradingterminal/SQLAgent.java
@@ -30,9 +30,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Time;
-import java.util.Date;
 import java.util.LinkedList;
 
 public class SQLAgent {
