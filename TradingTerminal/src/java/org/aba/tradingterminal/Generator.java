d2f1950d698ac3a609f82e4b4723110bfc828b34
kramer98489 <kramer98489@gmail.com>
2014-03-30 02:07:30 +0400

    Generator fixed

--- TradingTerminal/src/java/org/aba/tradingterminal/Generator.java
+++ TradingTerminal/src/java/org/aba/tradingterminal/Generator.java
@@ -34,21 +34,21 @@ import java.util.Random;
  */
 public class Generator {
 
-    private LinkedList<Product> RangeOfGoods;
-    private int AVGGoodsCount;
-    private int size;
+    private final LinkedList<Product> RangeOfGoods;
+    private final int AVGGoodsCount;
+    private final int size;
 
     public Generator(int avggoodscount) {
-        if (SQLAgent.TestConnect()) {
+     //   if (SQLAgent.TestConnect()) {
             RangeOfGoods = SQLAgent.GetProductInfo();
-        }
+       // }
         if (avggoodscount < RangeOfGoods.size()) {
             AVGGoodsCount = avggoodscount;
         } else {
             AVGGoodsCount = RangeOfGoods.size();
         }
         size = RangeOfGoods.size();
-    }
+    } 
 
     public Buyer CreateBuyer() {
         Random RNG = new Random();
@@ -61,7 +61,7 @@ public class Generator {
         // for (int i = 0, goodscount = Dist.GetIntCount(AVGGoodsCount); i < goodscount; i++) {
         for (int i = 0, goodscount = RangeOfGoods.size(); i < goodscount; i++) {
             if (RNG.nextInt(goodscount) < AVGGoodsCount) {
-                tmpProduct = RangeOfGoods.get(i);
+                tmpProduct =new Product();
                 if (tmpProduct.IsPacked) {
                     tmpProduct.Count = Dist.GetIntCount(tmpProduct.Count);
                 } else {
--- TradingTerminal/src/java/org/aba/tradingterminal/TTerminal.java
+++ TradingTerminal/src/java/org/aba/tradingterminal/TTerminal.java
@@ -372,7 +372,7 @@ public class TTerminal extends HttpServlet {
                                         workers.remove(i);
                                     }
                                 }
-                                if (IsConfigured && request.getParameterNames().hasMoreElements()) {
+                                if (IsConfigured && request.getParameterNames().hasMoreElements() && SQLAgent.TestConnect()) {
                                     String peoplecountstr, goodscountstr;
                                     peoplecountstr = request.getParameter("peoplecount");
                                     goodscountstr = request.getParameter("goodscount");
